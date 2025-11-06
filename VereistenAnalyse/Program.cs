4d842e4171e9136a5814d69a7a844b9c15b1869a
Masume Demirtas <12502779@student.pxl.be>
2025-11-06 13:34:36 +0100

    Vereistenanalyse

---
+++ VereistenAnalyse/Program.cs
@@ -0,0 +1,20 @@
+﻿static void Main()
+{
+    const string user = "Admin", password = "P@sswoord123";
+    int tries = 0;
+    while (tries < 3)
+    {
+        Console.Write("Gebruikersnaam: ");
+        string inputUser = Console.ReadLine();
+        Console.Write("Wachtwoord: ");
+        string inputPassword = Console.ReadLine();
+        if (inputUser == user && inputPassword == password)
+        {
+            Console.WriteLine($"Welkom, {inputUser}!");
+            return;
+        }
+        tries++;
+        Console.WriteLine($"Foutieve login.\n{3 - tries} pogingen over.");
+    }
+    Console.WriteLine("Te veel pogingen. Toegang geweigerd.");
+}
